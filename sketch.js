3b4650aad9ab8f692da993f37bb845264152abbd
matteogara <matteo.garagiola@outlook.com>
2026-03-26 17:44:37 +0100

    colore

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 let SIZE = 150;
-let NUMBER_OF_CIRCLES = 200;
+let NUMBER_OF_CIRCLES = 400;
 let AMPLITUDE = 150;
 
 function setup() {
@@ -12,11 +12,18 @@ function setup() {
   for (let count = 0; count < NUMBER_OF_CIRCLES; count += 1) {
     // Operazioni
     let progress = count / NUMBER_OF_CIRCLES; // Valore normalizzato (0 - 1)
-    
-    
+
+    // Posizione
     let centralX = width / 2;
     let x = sin(progress * 10) * AMPLITUDE + centralX;
     let y = height * progress;
+
+    // Colore
+    let r = 127 + sin(progress * 3) * 127;
+    let g = 127 + sin(progress * 10) * 127;
+    let b = 127 + sin(progress * 13) * 127;
+    fill(r, g, b);
+
     circle(x, y, SIZE);
   }
 }
