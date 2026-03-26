c0059c681dc88fc5566efeb2f6af0b5bc6c68563
matteogara <matteo.garagiola@outlook.com>
2026-03-26 17:08:52 +0100

    linea retta fatta da cerchi

--- sketch.js
+++ sketch.js
@@ -1,7 +1,19 @@
-function setup() {
-  createCanvas(400, 400);
-}
+let SIZE = 150;
+let NUMBER_OF_CIRCLES = 200;
 
-function draw() {
-  background(220);
+function setup() {
+  createCanvas(800, 800);
+  background(0);
+  
+  noStroke();
+  
+  // for (Valore di partenza; condizione per cui il for va avanti; come camia il valore ad ogni ciclo)
+  for (let count = 0; count < NUMBER_OF_CIRCLES; count += 1) {
+    // Operazioni
+    let progress = count / NUMBER_OF_CIRCLES; // Valore normalizzato (0 - 1)
+    
+    let x = width * progress;
+    let y = height / 2;
+    circle(x, y, SIZE);
+  }
 }
